c41dbaa7d1556acde693455eba26935cb4f7ed5e
Arthur Aguilar <arthurg.programming@gmail.com>
2024-02-12 14:06:08 -0300

    Refiz o script sem colar :D

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,35 +6,32 @@ result = document.querySelector('.result')
 
 btnAll.forEach((btn) => {
     btn.addEventListener('click', () => {
-        let value = btn.textContent
-        result.value += value
+        result.value += btn.textContent
     })
 })
+
+btnEqual.addEventListener('click', () => {
+    let math = result.value
+
+    if (result.value !== '') {
+        try {
+            result.value = eval(math)
+        } 
+        catch(error) {
+            console.error(`Error: ${error}`)
+            console.log('Deu erro aqui!')
+        }
+    } else {
+        alert('Por favor, digite um número primeiro!')
+    }
+})
+    
+
 
 btnClear.addEventListener('click', () => {
     result.value = ''
 })
 
 btnBackspace.addEventListener('click', () => {
-    if (result.value > 0) {
-        result.value = result.value.slice(0, -1)
-    }
+    result.value = result.value.slice(0, -1)
 })
-
-btnEqual.addEventListener('click', () => {
-    let equation = result.value
-
-    if (equation !== '') {
-        try {
-            result.value = eval(equation)
-        }
-        catch(err) {
-            console.error('Deu erro' + err)
-            alert('Digite a expressão corretamente!')
-        }
-    } else {
-        alert('Por favor, digite a expressão')
-    }
-})
-
-
